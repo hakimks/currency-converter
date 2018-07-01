00b0dd43f842d7d0ef33a491933ba11bb298ab4f
hakimks <hakimks@gmail.com>
2018-07-01 12:34:57 +0300

    indexed db storing rates

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ function openDatabase() {
     }
   
     return idb.open('kimconvert', 1, function(upgradeDb) {
-      var store = upgradeDb.createObjectStore('currency', {keyPath: "id"});
+      let store = upgradeDb.createObjectStore('currency', {keyPath: "id"});
+      let store2 = upgradeDb.createObjectStore('rate', {keyPath: "id"});
     });
   }
 
@@ -33,38 +34,68 @@ if ('serviceWorker' in navigator) {
   
 
 // get all currencies available
-fetch('https://free.currencyconverterapi.com/api/v5/currencies')
+// first check db and then get from nextwork
+fetchCurrency();
+// getCurrencies();
+
+function getCurrencies(){
+    if (dbPromise){
+        dbPromise.then(db => {
+            let tx = db.transaction('currency', 'readwrite');
+            let store = tx.objectStore('currency');
+            return store.openCursor();
+        }).then(function readCusrsor(cusr){
+            if (!cusr){
+                return;
+            }
+            select_orig_currency = document.getElementById('from_currency');
+            select_to_currency = document.getElementById('to_currency');
+            let id = cusr.value.id;
+            let name = cusr.value.name;
+    
+            select_orig_currency.add(new Option(name, id));
+            select_to_currency.add(new Option(name, id));
+    
+            cusr.continue().then(readCusrsor);
+        })
+    }
+}
+
+
+function fetchCurrency(){   
+    fetch('https://free.currencyconverterapi.com/api/v5/currencies')
     .then(response=> {
-       return response.json();
+    return response.json();
     }).then(data =>{
+        // if no data, get currencies from database
+        if(!data){
+            getCurrencies();
+            return;
+        }
     //   console.log(data);
     //    const currencies = data.results;
     //   for( const currency in currencies){
     //      console.log(currency);
-          
+        
     //   }  
 
     // console.log(Object.entries(data.results));
     const currencyArray = Object.entries(data.results);
     let testmap = new Map();
     for(const currency of currencyArray){
-     //   console.log(currency);
+    //   console.log(currency);
         let currencyName = currency[1].currencyName;
-       // let currencySymbole = currency[1].currencySymboll
+    // let currencySymbole = currency[1].currencySymboll
         let currencyId = currency[1].id;
 
         testmap.set(currency[1].id, currency[1].currencyName);
         
-       // currencies.add(currencyId, currencyName);
+    // currencies.add(currencyId, currencyName);
         // console.log(currencyId);
         // console.log(currencyName);
-        // console.log(testmap.size);
-        
-        
-        
+        // console.log(testmap.size);     
     }
-    return testmap;
-        
+    return testmap;      
     })
     .then(currencyMap =>{
         select_orig_currency = document.getElementById('from_currency');
@@ -74,10 +105,8 @@ fetch('https://free.currencyconverterapi.com/api/v5/currencies')
             let[id, name] = curr;
         //    console.log(id);
         //    console.log(name);
-           select_orig_currency.add(new Option(name, id));
-           select_to_currency.add(new Option(name, id));
-
-            
+        select_orig_currency.add(new Option(name, id));
+        select_to_currency.add(new Option(name, id));      
         }
         // add this to the indexedBD the currency object store
         dbPromise.then(db => {
@@ -88,47 +117,98 @@ fetch('https://free.currencyconverterapi.com/api/v5/currencies')
                 let obj = {id, name};
                 store.put(obj);        
             }
-        })
-
-        
+        }) 
     })
     .catch(err => {
         console.log("There was an error .", err);
     })
 
-    
-    // submit request
-    // https://free.currencyconverterapi.com/api/v5/convert?q=USD_PHP
-    // url = 
-    // fetch()
+}
+
+// submit request
+// https://free.currencyconverterapi.com/api/v5/convert?q=USD_PHP
+// url = 
+// fetch()
+function submitQuery(fromField, toField){
+    urlQuery =  'https://free.currencyconverterapi.com/api/v5/convert?q='
+    this.fromField = fromField;
+    this.toField = toField;
+    let tofromPart = fromField+'_'+toField;
+    queryString = urlQuery + tofromPart;
+
+    //   console.log(queryString);
+    fetch(queryString)
+    .then(response =>{
+        return response.json();
+    }).then(data => {
+        // if no data, check database
+        if(!data){
+            let rate = 0;
+            getRateFromDatabase(tofromPart);
+
+            return rate;
+        }
+        // console.log(data);
+        const qResults = Object.entries(data.results);
+        //  console.log(qResults);
+        // console.log(qResults[0][1].val);
+        let rate = qResults[0][1].val;
+        let id = qResults[0][1].id;
+        let to = qResults[0][1].to;
+        let fr = qResults[0][1].fr;
+        // Store results in rate object
+        dbPromise.then(db => {
+            let tx = db.transaction('rate', 'readwrite');
+            let store = tx.objectStore('rate');
+            let obj = {id, rate, to, fr};
+            store.put(obj);        
+            
+        }) 
+
+
+        return rate;
+        
+    }).then(rate => {
+        if(rate ===0) {
+            document.getElementById('to_amount').value = "Need Internet";
+            return;
+        }
+        let amountField = document.getElementById('from_amount').value;
+        let convertedValue = rate * amountField;
+        document.getElementById('to_amount').value = convertedValue;
+        
+
+    }).catch(()=>{
+        document.getElementById('to_amount').value = "Need Internet";
+    })
+}
 
 const form_element = document.getElementById('currency-form');
 form_element.addEventListener('submit', event => {
   event.preventDefault();
   let fromField = document.getElementById('from_currency').value;
   let toField = document.getElementById('to_currency').value;
-//   let amountField = document.getElementById('from_amount').value;
-   urlQuery =  'https://free.currencyconverterapi.com/api/v5/convert?q='
-   queryString = urlQuery+fromField+'_'+toField;
- 
-//   console.log(queryString);
-    fetch(queryString)
-    .then(response =>{
-        return response.json();
-    }).then(data => {
-        // console.log(data);
-        const qResults = Object.entries(data.results);
-        //  console.log(qResults);
-        // console.log(qResults[0][1].val);
-        let rate = qResults[0][1].val;
-        return rate;
-        
-    }).then(rate => {
-        let amountField = document.getElementById('from_amount').value;
-        let convertedValue = rate * amountField;
-        document.getElementById('to_amount').value = convertedValue;
-    })
+
+  submitQuery(fromField, toField);
+   
+
+
 });
+
+
+function getRateFromDatabase(tofromPart) {
+    this.tofromPart = tofromPart;
+    dbPromise.then(db => {
+        let tx = db.transaction('rate', 'readwrite');
+        let store = tx.objectStore('rate');
+        return store.getAll(tofromPart);
+    }).then(obj => {
+        if(!obj){
+            return 0;
+        }
+        return obj.rate;
+    })
+}
 
 // Index Db transactions , add currency id and name 
 // dbPromise.then((db) => {
